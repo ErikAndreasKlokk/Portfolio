370f9d58e84418df70472f0e57c97355b67428f9
ErikAndreasKlokk <eaklokk@gmail.com>
2024-02-28 09:39:00 +0100

    started with the portfolio and setup astro with svelte and daisyUI

---
+++ src/components/header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<header class=" w-full flex justify-end">
+    <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost rounded-btn"><img src="/language_FILL0_wght400_GRAD0_opsz24.svg" alt="Select language"></div>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><button>Norsk</button></li> 
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <li><button>English</button></li>
+        </ul>
+      </div>
+</header>
