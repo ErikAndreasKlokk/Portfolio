c470499641a49351dafb77fa08354c813cc1d424
ErikAndreasKlokk <eaklokk@gmail.com>
2024-02-29 10:35:14 +0100

    woohoo

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,11 +1,10 @@
-<script lang="ts">
+<script>
     import { language } from '../lib/stores/languages'
 
-    function changeLang(lang: string) {
+    function changeLang(lang) {
         language.set({
           language: lang
         })
-        console.log("Changed language")
     }
 </script>
 
@@ -13,7 +12,7 @@
     <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost rounded-btn"><img src="/language_FILL0_wght400_GRAD0_opsz24.svg" alt="Select language"></div>
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+        <ul tabindex="0" class="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-48 mt-4">
           <!-- svelte-ignore a11y-missing-attribute -->
           <li><button on:click={() => changeLang("Norsk")}>Norsk</button></li> 
           <!-- svelte-ignore a11y-missing-attribute -->
