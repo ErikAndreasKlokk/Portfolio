1a71a19b26fa24ee6fbcf07413ff08a226cbd3bf
ErikAndreasKlokk <eaklokk@gmail.com>
2024-11-13 23:58:37 +0100

    Made a sideProfile component

---
+++ src/lib/components/sideProfile.svelte
@@ -0,0 +1,29 @@
+<div class=" flex flex-col w-1/2 h-fit sticky px-5 top-56 breakpoint:static breakpoint:mb-5 breakpoint:items-center breakpoint:w-fit">
+    <h1 class=" font-bold text-6xl mb-4 breakpoint:text-center">ERIK ANDREAS KLOKK</h1>
+    <div class=" border flex flex-col border-white/60 rounded-md px-3 py-1 w-4/5 breakpoint:w-full breakpoint:my-1">
+        <div class=" flex justify-between ">
+            <p class=" flex text-white/60 text-sm gap-1 items-center">
+                <img class=" h-4" src="/location.svg" alt="location">
+                Oslo, Norway
+            </p>
+            <div class=" flex gap-4 py-2">
+                <a href="mailto:eaklokk@gmail.com">
+                    <img class=" h-6" src="/mail.svg" alt="new mail">
+                </a>
+                <a target="_blank" href="https://github.com/ErikAndreasKlokk">
+                    <img class=" h-6" src="/github.svg" alt="github link">
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/erik-andreas-klokk-5415882a0/">
+                    <img class=" h-6" src="/linkedin.svg" alt="linkedin link">
+                </a>
+                <a target="_blank" href="https://steamcommunity.com/profiles/76561198848256576/">
+                    <img class=" h-6" src="/steam.svg" alt="steam link">
+                </a>
+            </div>
+        </div>
+        <p class=" text-lg my-1">18 year old frontend web developer working at Statens Pensjonskasse.</p>
+        <div class=" w-full flex justify-end pb-1 pr-1">
+            <a class=" text-sm text-white/60 underline" href="/wishlist">See my wishlist</a>
+        </div>
+    </div>
+</div>
