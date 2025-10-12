1edd5ebce88dd15a4f8d9969326c23285c2b202f
Erik Andreas Klokk <eaklokk@gmail.com>
2025-10-12 23:50:14 +0200

    feat: trying to fix SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,7 @@
 
 <svelte:head >
     <title>Erik Andreas Klokk</title>
+    <meta name="description" content="Erik Andreas Klokk's personal website. Learn about my interests, hobbies and projects!">
 </svelte:head>
 
 <div class=" max-w-[1200px] w-full breakpoint:max-w-[600px]">
